5086b5d790407fe76727c5e69a7d629418824925
VICTOR <victor_1_11@hotmail.com>
2017-01-31 10:22:28 -0500

    camera

--- app/src/main/java/odin/android/imagefirebase/MainActivity.java
+++ app/src/main/java/odin/android/imagefirebase/MainActivity.java
@@ -4,12 +4,14 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -25,6 +27,7 @@ import com.squareup.picasso.Picasso;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -43,6 +46,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private StorageReference Storage;
 
     private Uri filepath;
+    private Uri filepath2;
+    private   Bundle extras;
 
 
     @Override
@@ -81,26 +86,91 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 e.printStackTrace();
             }
 
-        } else if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK && data != null && data.getData() != null) {
-
-
-            filepath = data.getData();
-           try {
-               Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filepath);
-               imageView.setImageBitmap(bitmap);
-
-           } catch (IOException e) {
-               e.printStackTrace();
+        } else if (requestCode == CAMERA_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+           if(filepath2 != null){
+               Uri selectedImage = filepath2;
+               getContentResolver().notifyChange(selectedImage, null);
+               Bitmap reducedSizeBitmap = getBitmap(filepath2.getPath());
+               if(reducedSizeBitmap != null){
+                   imageView.setImageBitmap(reducedSizeBitmap);
+                   Button uploadImageButton = (Button) findViewById(R.id.send);
+                   uploadImageButton.setVisibility(View.VISIBLE);
+               }else{
+                   Toast.makeText(this,"Error while capturing Image",Toast.LENGTH_LONG).show();
+               }
+           }else{
+               Toast.makeText(this,"Error while capturing Image",Toast.LENGTH_LONG).show();
            }
-        }
+       }
 
 
 
     }
 
+    private Bitmap getBitmap(String path) {
+
+        Uri uri = Uri.fromFile(new File(path));
+        InputStream in = null;
+        try {
+            final int IMAGE_MAX_SIZE = 1200000; // 1.2MP
+            in = getContentResolver().openInputStream(uri);
+
+            // Decode image size
+            BitmapFactory.Options o = new BitmapFactory.Options();
+            o.inJustDecodeBounds = true;
+            BitmapFactory.decodeStream(in, null, o);
+            in.close();
+
+
+            int scale = 1;
+            while ((o.outWidth * o.outHeight) * (1 / Math.pow(scale, 2)) >
+                    IMAGE_MAX_SIZE) {
+                scale++;
+            }
+            Log.d("", "scale = " + scale + ", orig-width: " + o.outWidth + ", orig-height: " + o.outHeight);
+
+            Bitmap b = null;
+            in = getContentResolver().openInputStream(uri);
+            if (scale > 1) {
+                scale--;
+                // scale to max possible inSampleSize that still yields an image
+                // larger than target
+                o = new BitmapFactory.Options();
+                o.inSampleSize = scale;
+                b = BitmapFactory.decodeStream(in, null, o);
+
+                // resize to desired dimensions
+                int height = b.getHeight();
+                int width = b.getWidth();
+                Log.d("", "1th scale operation dimenions - width: " + width + ", height: " + height);
+
+                double y = Math.sqrt(IMAGE_MAX_SIZE
+                        / (((double) width) / height));
+                double x = (y / height) * width;
+
+                Bitmap scaledBitmap = Bitmap.createScaledBitmap(b, (int) x,
+                        (int) y, true);
+                b.recycle();
+                b = scaledBitmap;
+
+                System.gc();
+            } else {
+                b = BitmapFactory.decodeStream(in);
+            }
+            in.close();
+
+            Log.d("", "bitmap size - width: " + b.getWidth() + ", height: " +
+                    b.getHeight());
+            return b;
+        } catch (IOException e) {
+            Log.e("", e.getMessage(), e);
+            return null;
+        }
+    }
+
     private void uploadFile()
     {
-        if (filepath !=null) {
+        if (filepath !=null ) {
             clicks++;
 
             final ProgressDialog progressDialog = new ProgressDialog(this);
@@ -133,6 +203,43 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
             });
         }
+        else if (filepath2 !=null )
+        {
+            clicks++;
+
+
+
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Cargando...");
+            progressDialog.show();
+            StorageReference riversRef = Storage.child("images/foto N:"+clicks+"");
+
+            riversRef.putFile(filepath2)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            // Get a URL to the uploaded content
+                            progressDialog.dismiss();
+                            Toast.makeText(getApplicationContext(), "Archivo cargado", Toast.LENGTH_LONG).show();
+
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            progressDialog.dismiss();
+                            Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+
+                    double progress=(100.0*taskSnapshot.getBytesTransferred())/taskSnapshot.getTotalByteCount();
+                    progressDialog.setMessage(((int) progress)+"% Cargando...");
+                }
+            });
+
+        }
     }
 
     private void  showFileImage()
@@ -155,10 +262,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void cameraFileImage()
     {
 
-        final Intent intent = new Intent(
-                MediaStore.ACTION_IMAGE_CAPTURE);
+       /* final Intent intent = new Intent(
+                android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        getParent().startActivityForResult(intent, CAMERA_REQUEST_CODE);
 
-        startActivityForResult(intent, CAMERA_REQUEST_CODE);
+*/
+      /*  Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
+
+        }*/
+
+
+
+        Intent chooserIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File f = new File(Environment.getExternalStorageDirectory(), "POST_IMAGE.jpg");
+        chooserIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+        filepath2 = Uri.fromFile(f);
+        startActivityForResult(chooserIntent, CAMERA_REQUEST_CODE);
 
     }
 
